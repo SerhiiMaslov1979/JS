0ba0b626071f7fb8dbae001d0a21f491e1d96514
SerhiiMaslov1979 <112109122+SerhiiMaslov1979@users.noreply.github.com>
2023-11-26 21:30:05 +0200

    1-1

---
+++ js/Lesson_1/script.js
@@ -0,0 +1,69 @@
+// let value;
+// console.log(value);
+// value = 1;
+// console.log(value);
+
+// console.log(3 === "3");
+// console.log(3 == "3");
+
+// 1 undefined => false;
+// 2 null      => false;
+// 3 0         => false;
+// 4 NaN       => false;
+// 5 ' '       => false;
+// 6 false     => false;
+
+// console.log(Boolean("false")); =>
+// console.log(!!"false");
+
+// const a = 20;
+// console.log(a);
+
+// let b;
+// b = 15;
+
+// console.log(b);
+
+// Примштивні типи даних - імутабельні типи даних
+// 1 Number
+// 2 String
+// 3 Boolean
+// 4 Undefined
+// 5 Null
+
+// let a = 10;
+// a = a + 2;
+// a = 12;
+// a += 2;
+
+// console.log(a);
+
+
+// Example 1 - Матиматичні оператори
+// Виведи на екран загальну кількість яблук та винограду. Різницю яблук та винограду
+
+// const apples = 47;
+// const grapes = 135;
+// const total = apples + grapes;
+// console.log(total);
+// const diff = grapes - apples;
+// console.log(diff);
+
+// Example 2 - Комбіновані оператори
+// Заміни вираз перевизначення комбінованим оператором +=.
+
+// let students = 100;
+// students = students + 50;
+// students += 50;
+// console.log(students);
+
+// Example 3 - Пріоритет операторів
+// Розбери приоритети операторів в інструкції привласнення значення змінної result.
+
+// const result = 108 + 223 - 2 * 5;
+// console.log(result);
+
+// Example 4 - Клас Math
+// Напиши скрипт, який виводить у консолі заокруглені вгору/вниз і т.д. значення змінної
+// value. Використовуй метолди Math.floor(), Math.ceil(), та Math.round(). Перевір що буде в консолі при значеннях 27.3 та 27.9.
+
